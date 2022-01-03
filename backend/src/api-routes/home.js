184648bd560ea2f716b28aa5a19b53813834fb2b
ianlavine <71903686+ianlavine@users.noreply.github.com>
2022-01-03 17:57:29 -0500

    first connection made between tickerSearch in frontend and home.js in backend

--- backend/src/api-routes/home.js
+++ backend/src/api-routes/home.js
@@ -23,39 +23,20 @@ router.get('/info/', (req, res) => {
     }
 );
 
-<<<<<<< HEAD
-/**
- * Provide the post request with a body (ex. {ticker: "BTC"}) and it will return the information on that
- * ticker using the coinmarketcap api
- */
- router.post('/ticker2/', (req, res) => {
-    const tickerName = req.body.ticker;
-    const stringNeeded = "?CMC_PRO_API_KEY=" + 'dc223376-a632-41a9-8c70-b3ba8eb38f7f' + "&symbol=" + `${tickerName}`;
-=======
 router.post('/ticker/', (req, res) => {
     let tickerName = req.body.ticker;
     const apiKey = process.env.API_KEY;
     const stringNeeded = "?CMC_PRO_API_KEY=" + apiKey + "&symbol=" + tickerName;
->>>>>>> 17a1159ee03f8e96d6174248d44f9272b1ab6dde
     const url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest';
     const totalUrl = url + stringNeeded;
 
     axios.get(totalUrl)
         .then(response => {
-<<<<<<< HEAD
-            res.send(response.data);
-        })
-=======
     res.send(response.data);
     })
->>>>>>> 17a1159ee03f8e96d6174248d44f9272b1ab6dde
         .catch(error => {
             console.error(error)
         });
 });
 
-<<<<<<< HEAD
 module.exports = router;
-=======
-module.exports = router;
->>>>>>> 17a1159ee03f8e96d6174248d44f9272b1ab6dde
